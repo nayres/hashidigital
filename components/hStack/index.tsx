47494434da665750cbc972685312272d9e957dc8
NickAyres <Nickolasayres1!>
2022-04-14 18:42:25 -0700

    Add HStack

---
+++ components/hStack/index.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react'
+import styles from './style.module.css'
+import { StackAlignmentTypes, StackStyleAttributes } from 'components/types'
+import { getMappedStackStyles, makeClassName } from 'components/utils'
+import { hStackAlignmentOptions } from './hStackAlignmentOptions'
+
+type SemanticElements =
+  | 'article'
+  | 'aside'
+  | 'details'
+  | 'figcaption'
+  | 'figure'
+  | 'header'
+  | 'main'
+  | 'mark'
+  | 'nav'
+  | 'section'
+  | 'summary'
+  | 'figure'
+  | 'time'
+
+interface HStackProps {
+  /* @as: semantic element alias - 'article' | 'aside' | 'details' | 'main' | 'header' | etc */
+  _as?: SemanticElements | 'div'
+  /* @spacing: the space between stacked content */
+  spacing?: string
+  /* @align: Where in the parent the stack sits - 'left' | 'center' | 'right' */
+  align?: Exclude<StackAlignmentTypes, 'top' | 'bottom'>
+  /* @className: consumer can compose a className */
+  className?: string
+}
+
+const HStack: FC<HStackProps> = ({
+  _as = 'div',
+  spacing,
+  align = 'left',
+  className,
+  children,
+}) => {
+  const getClassNames = makeClassName(styles)
+  const alignItemsFromProps = getMappedStackStyles(
+    align,
+    hStackAlignmentOptions
+  )[StackStyleAttributes.ALIGN_ITEMS]
+  const justifyContentFromProps = getMappedStackStyles(
+    align,
+    hStackAlignmentOptions
+  )[StackStyleAttributes.JUSTIFY_CONTENT]
+
+  return (
+    <_as
+      className={getClassNames('baseHStack', className)}
+      style={{
+        gap: spacing,
+        alignItems: `${alignItemsFromProps}`,
+        justifyContent: `${justifyContentFromProps}`,
+      }}
+    >
+      {children}
+    </_as>
+  )
+}
+
+export default HStack
